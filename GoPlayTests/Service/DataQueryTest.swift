780d67440c4222d592c00fd701607c6f9a4dd6cb
Tifo <tifoaudi@ruangguru.com>
2021-06-30 21:30:57 +0700

    test: create unit test for MovieDataQuery object

---
+++ GoPlayTests/Service/DataQueryTest.swift
@@ -0,0 +1,77 @@
+//
+//  DataQueryTest.swift
+//  GoPlayTests
+//
+//  Created by Tifo Audi Alif Putra on 30/06/21.
+//
+
+@testable import GoPlay
+import XCTest
+
+class DataQueryTest: XCTestCase {
+
+    // MARK: - Mocks
+    final class NetworkServiceMock: NetworkService {
+        
+        var fetchMovieCalled = false
+        
+        func fetchMovie(for endpoint: MovieEndpoint, success: @escaping (MoviesResponse) -> Void, failure: @escaping (ErrorResponse) -> Void) {
+            fetchMovieCalled = true
+        }
+    }
+    
+    final class LocalRepositoryMock: LocalRepository {
+        
+        var saveCalled = false
+        var loadCalled = false
+        
+        func save(key: MovieEndpoint, movies: [Movie]) {
+            saveCalled = true
+        }
+        
+        func load(key: MovieEndpoint, onSuccess: @escaping ([Movie]) -> Void) {
+            loadCalled = true
+        }
+    }
+    
+    // MARK: - Testcases
+    
+    func testDataQueryShouldAskNetworkServiceToFetchMovie() {
+        // Given
+        let mockNetworkService = NetworkServiceMock()
+        let mockLocalRepository = LocalRepositoryMock()
+        
+        let dataQuery = MovieDataQuery(networkService: mockNetworkService, localRepository: mockLocalRepository)
+        
+        // When
+        dataQuery.fetchMovies(for: .nowPlaying) { _ in
+            XCTAssert(mockNetworkService.fetchMovieCalled)
+        } failure: { _ in }
+    }
+
+    func testDataQueryShouldAskLocalRepositoryToLoadMovie() {
+        // Given
+        let mockNetworkService = NetworkServiceMock()
+        let mockLocalRepository = LocalRepositoryMock()
+        
+        let dataQuery = MovieDataQuery(networkService: mockNetworkService, localRepository: mockLocalRepository)
+        
+        // When
+        dataQuery.loadMovies(for: .nowPlaying) { _ in
+            XCTAssert(mockLocalRepository.loadCalled)
+        }
+    }
+    
+    func testDataQueryShouldAskLocalRepositoryToSaveMovie() {
+        // Given
+        let mockNetworkService = NetworkServiceMock()
+        let mockLocalRepository = LocalRepositoryMock()
+        
+        let dataQuery = MovieDataQuery(networkService: mockNetworkService, localRepository: mockLocalRepository)
+        
+        // When
+        dataQuery.fetchMovies(for: .nowPlaying) { _ in
+            XCTAssert(mockLocalRepository.saveCalled)
+        } failure: { _ in }
+    }
+}
