d43fa496f07faf0fb6aba74a4bf442df7595e65f
Tifo <tifoaudi@ruangguru.com>
2021-06-30 14:28:26 +0700

    feat: create app tab bar controller

--- GoPlay/Scenes/MainTabBar/MainAppViewController.swift
+++ GoPlay/Scenes/MainTabBar/MainAppViewController.swift
@@ -11,13 +11,48 @@ final class MainAppViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        delegate = self
+        configureTabView()
+    }
+    
+    private func configureTabView() {
+        view.backgroundColor = .white
+        viewControllers = createViewControllers()
+        tabBar.backgroundColor = .white
+    }
+    
+    private func createViewControllers() -> [UIViewController] {
+        let movieListViewController = createMovieListViewController()
+        let searchMovieViewController = createSearchMovieViewController()
+        return [movieListViewController, searchMovieViewController]
+    }
+    
+    private func createMovieListViewController() -> UINavigationController {
+        let dataQuery = MovieDataQuery(
+            networkService: MovieNetworkService()
+        )
+        
+        let interactor = MovieListDefaultInteractor(
+            dataQuery: dataQuery
+        )
+        
+        let viewModel = MovieListDefaultViewModel(
+            interactor: interactor
+        )
+        
+        let viewController: MovieListViewController = MovieListViewController(
+            viewModel: viewModel
+        )
+        
+        let movieListItemBar: UITabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 0)
+        viewController.tabBarItem = movieListItemBar
+        return UINavigationController(rootViewController: viewController)
+    }
+    
+    private func createSearchMovieViewController() -> UINavigationController {
+        let viewController: SearchMovieViewController = SearchMovieViewController()
+        let searchMovieItemBar: UITabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 1)
+        viewController.tabBarItem = searchMovieItemBar
+        return UINavigationController(rootViewController: viewController)
     }
 }
 
-extension MainAppViewController: UITabBarControllerDelegate {
-    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-        true
-    }
-}
